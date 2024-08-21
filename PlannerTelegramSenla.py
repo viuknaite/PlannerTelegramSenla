71016cb1ba3436042f9a57db08b04b288fe7fecb
Victoria <viuknaite@gmail.com>
2024-08-21 23:46:41 +0300

    new changes

---
+++ PlannerTelegramSenla.py
@@ -0,0 +1,25 @@
+import telebot
+from telebot.types import Message
+import json
+
+bot_client = telebot.TeleBot(token="7175797246:AAEeYWrUkHRl7zAbwV-q7fRr_6W0n-9vHT4")
+ADMIN_CHAT_ID = 5218304059
+
+
+@bot_client.message_handler(commands=["start"])
+def echo(message: Message):
+    with open("users.json", "r") as f_o:
+        data_from_json = json.load(f_o)
+
+    user_id = message.from_user.id
+    username = message.from_user.username
+
+    if str(user_id) not in data_from_json:
+        data_from_json[user_id] = {"username": username}
+
+    with open("users.json", "w") as f_o:
+        json.dump(data_from_json, f_o, indent=4, ensure_ascii=False)
+    bot_client.reply_to(message=message, text=str(f"Регистрация успешна, {username}"))
+
+
+bot_client.polling()
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # PlannerTelegramSenla
 
 Telegram bot for task and time tracking
+
+pyTelegramBotAPI==4.22.1
